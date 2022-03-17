d3d1e07a4086889ddd1fcfbf0d8671301f3aee33
Dzmitry Lamaka <lamaka.dima@gmail.com>
2022-03-17 21:50:04 +0300

    add localization for pages

--- src/main/java/by/lamaka/springmvc/config/Config.java
+++ src/main/java/by/lamaka/springmvc/config/Config.java
@@ -2,12 +2,18 @@ package by.lamaka.springmvc.config;
 
 import com.github.fluent.hibernate.cfg.scanner.EntityScanner;
 import org.hibernate.SessionFactory;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 
 @Configuration
@@ -23,9 +29,24 @@ public class Config {
     }
 
     @Bean
-    public static SessionFactory sessionFactory(){
+    public static SessionFactory sessionFactory() {
         org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
         EntityScanner.scanPackages("by.lamaka.springmvc.entity").addTo(configuration);
         return configuration.buildSessionFactory();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        final SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en", "US"));
+        return resolver;
     }
 }
--- src/main/webapp/pages/error.jsp
+++ src/main/webapp/pages/error.jsp
@@ -1,12 +1,12 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
 <html>
 <head>
     <title>Error page</title>
 </head>
 <body>
-<h2>Error</h2>
-Error message : ${message}<br><br>
-Details : ${details}
-
+<h2><s:message code="label.error"/></h2>
+<s:message code="label.error.message"/> : ${message}<br><br>
+<s:message code="label.error.details"/> : ${details}
 </body>
 </html>
